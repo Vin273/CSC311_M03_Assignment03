336507024d68dd3e225f58dbae7b4be4ebb49d0a
soogrlyi <angieportillo601@gmail.com>
2026-03-06 19:57:31 -0500

    tabs

--- src/main/java/edu/farmingdale/csc311_m03_assignment03/MazeApplication.java
+++ src/main/java/edu/farmingdale/csc311_m03_assignment03/MazeApplication.java
@@ -8,12 +8,14 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import java.io.IOException;
 import javafx.scene.Group;
-
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
 public class MazeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MazeApplication.class.getResource("maze-view.fxml"));
+
         Image maze1 = new Image("maze1.png");
         ImageView imageView1 = new ImageView(maze1);
         imageView1.setFitWidth(700);
@@ -25,20 +27,30 @@ public class MazeApplication extends Application {
         imageView2.setY(300);
 
         Group root = new Group(imageView1, imageView2);
-        Scene scene = new Scene(root, 700, 500);
 
+        Image maze2 = new Image("maze2.png");
+        ImageView mazeImageView2 = new ImageView(maze2);
+        mazeImageView2.setFitWidth(700);
+        mazeImageView2.setFitHeight(500);
+        Car car1 = new Car();
+        Group group = new Group(mazeImageView2, car1);
 
-//        Image maze2 = new Image("maze2.png");
-//        ImageView mazeImageView2 = new ImageView(maze2);
-//        mazeImageView2.setFitWidth(700);
-//        mazeImageView2.setFitHeight(500);
-//        Car car1 = new Car();
-//        Group group = new Group(mazeImageView2, car1);
-//        Scene scene = new Scene(group);
+        Tab tab1 = new Tab("Maze 1");
+        tab1.setContent(root);
+        tab1.setClosable(false);
+
+        Tab tab2 = new Tab("Maze 2");
+        tab2.setContent(group);
+        tab2.setClosable(false);
+
+        TabPane tabPane = new TabPane();
+        tabPane.getTabs().addAll(tab1, tab2);
+
+        Scene scene = new Scene(tabPane, 700, 500);
 
         final int MOVE = 5;
 
-        scene.setOnKeyPressed(event -> {
+        scene.addEventFilter(javafx.scene.input.KeyEvent.KEY_PRESSED, event -> {
 
             double x = imageView2.getX();
             double y = imageView2.getY();
@@ -47,30 +59,35 @@ public class MazeApplication extends Application {
 
                 case LEFT:
                     imageView2.setX(x - MOVE);
+                    event.consume();
                     break;
 
                 case RIGHT:
                     if (x + MOVE <= 700 - imageView2.getImage().getWidth()) {
-                        imageView2.setX(x + MOVE );
+                        imageView2.setX(x + MOVE);
                     }
+                    event.consume();
                     break;
 
                 case UP:
                     imageView2.setY(y - MOVE);
+                    event.consume();
                     break;
 
                 case DOWN:
                     imageView2.setY(y + MOVE);
+                    event.consume();
                     break;
             }
-            
         });
 
         stage.setTitle("Maze");
         stage.setScene(scene);
         stage.show();
 
+        root.requestFocus();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
